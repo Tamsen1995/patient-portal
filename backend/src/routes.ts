5d3cc0e73653f5b234a8b7deb50fd025b8f865b4
Tamsen1995 <tamanhbui95@gmail.com>
2024-03-25 12:36:40 -0400

    User can add a new temperature for any given date

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -22,6 +22,40 @@ router.get("/patients/:id", async (req: Request, res: Response) => {
   res.json(patient);
 });
 
+router.post(
+  "/patients/:id/temperatures",
+  async (req: Request, res: Response) => {
+    const { temperature, date } = req.body;
+    const { id } = req.params;
+
+    if (!temperature || !date) {
+      return res
+        .status(400)
+        .json({ error: "Temperature and date are required" });
+    }
+
+    try {
+      const patient = await Patient.findByPk(id);
+
+      if (!patient) {
+        return res.status(404).json({ error: "Patient not found" });
+      }
+
+      const newTemperature = await BodyTemperature.create({
+        temperature,
+        date,
+        PatientId: id,
+      });
+      console.log("newTemperature : ", newTemperature);
+      res.json(newTemperature);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "An error occurred while adding the temperature" });
+    }
+  }
+);
+
 router.post("/patients", async (req: Request, res: Response) => {
   const patient = await Patient.create(req.body, {
     include: [Medication, BodyTemperature],
